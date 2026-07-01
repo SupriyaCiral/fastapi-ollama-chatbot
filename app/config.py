d9cab8c8bb4d0a448c028b06b2a726d6ca74d622
Supriyabmscee <supriyap.mel@bmsce.ac.in>
2026-07-01 11:48:13 +0530

    Docker image created

--- app/config.py
+++ app/config.py
@@ -1,7 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     OLLAMA_BASE_URL: str = "http://localhost:11434"
     MODEL_NAME: str = "llama3:8b"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
 
 settings = Settings()
